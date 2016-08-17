d0cf000b67a75cd6de94fe85ffdf807884a0c3f1
Alice Tu <alice@Alices-MacBook-Air.local>
2016-08-17 14:42:27 -0700

    add portfolio, in progress

--- js/script.js
+++ js/script.js
@@ -1,28 +1,18 @@
-var switchPortfolioToActive = function(){
-	// remove 'active' from Home button
-	var classes = document.querySelector("#menuHome").className;
-	classes = classes.replace(new RegExp("active", "g"), "");
-	document.querySelector("#menuHome").className = classes;
 
-	// add 'active' class to Menu button
-	classes = document.querySelector("#menuPortfolio").className;
-	if (classes.indexOf("active") == -1){
-		classes += " active";
-		document.querySelector("#menuPortfolio").className = classes;
-	}
-};
+function disableLightbox(){
+	document.getElementsByTagName("figure")[0].getElementsByTagName("A")[0].removeAttribute("data-lightbox");
+}
 
+function disableImage(){
+	document.getElementsByTagName("figure")[0].getElementsByTagName("A")[0].removeAttribute("href");
+}
 
-var switchGalleryToActive = function(){
-	// remove 'active' from Home button
-	var classes = document.querySelector("#menuHome").className;
-	classes = classes.replace(new RegExp("active", "g"), "");
-	document.querySelector("#menuHome").className = classes;
+var windowWidth = $(window).width();
+if (windowWidth < 767){
+	disableLightbox();
+}
 
-	// add 'active' class to Menu button
-	classes = document.querySelector("#menuGallery").className;
-	if (classes.indexOf("active") == -1){
-		classes += " active";
-		document.querySelector("#menuGallery").className = classes;
-	}
-};
+// if ( $(window).width() < 767){
+// 	disableLightbox();
+// 	disableImage();
+// }
