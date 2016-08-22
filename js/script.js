2b8276d42374d7a4588a6e784aef8f73a57c07b0
Alice Tu <alice@Alices-MacBook-Air.local>
2016-08-22 12:48:08 -0700

    responsive design CSS added for proj page

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
 
-function disableLightbox(){
-	document.getElementsByTagName("figure")[0].getElementsByTagName("A")[0].removeAttribute("data-lightbox");
-}
+// function disableLightbox(){
+// 	document.getElementsByTagName("figure")[0].getElementsByTagName("A")[0].removeAttribute("data-lightbox");
+// }
 
-function disableImage(){
-	document.getElementsByTagName("figure")[0].getElementsByTagName("A")[0].removeAttribute("href");
-}
+// function disableImage(){
+// 	document.getElementsByTagName("figure")[0].getElementsByTagName("A")[0].removeAttribute("href");
+// }
 
-var windowWidth = $(window).width();
-if (windowWidth < 767){
-	disableLightbox();
-}
+// var windowWidth = $(window).width();
+// if (windowWidth < 767){
+// 	disableLightbox();
+// }
 
 // if ( $(window).width() < 767){
 // 	disableLightbox();
 // 	disableImage();
 // }
+
+
+
+
+Lightbox.prototype.enable = function() {
+    var self = this;
+
+    if $(window).width(>768){
+	    $('body').on('click', 'a[rel^=lightbox], area[rel^=lightbox], a[data-lightbox], area[data-lightbox]', function(event) {
+	      self.start($(event.currentTarget));
+	      return false;
+	    });
+	}
+	else{
+		return;
+	}
+  };
