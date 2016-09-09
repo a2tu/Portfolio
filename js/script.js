a2783eac1c97cf4b5d4106499ec3dc54f7e875ea
Alice Tu <alice@Alices-MacBook-Air.local>
2016-09-09 12:40:33 -0600

    text change

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 
-// function disableLightbox(){
-// 	document.getElementsByTagName("figure")[0].getElementsByTagName("A")[0].removeAttribute("data-lightbox");
-// }
+function disableLightbox(){
+	document.getElementsByTagName("figure")[0].getElementsByTagName("A")[0].removeAttribute("data-lightbox");
+}
 
-// function disableImage(){
-// 	document.getElementsByTagName("figure")[0].getElementsByTagName("A")[0].removeAttribute("href");
-// }
+function disableImage(){
+	document.getElementsByTagName("figure")[0].getElementsByTagName("A")[0].removeAttribute("href");
+}
 
-// var windowWidth = $(window).width();
-// if (windowWidth < 767){
-// 	disableLightbox();
-// }
+var windowWidth = $(window).width();
+if (windowWidth < 767){
+	disableLightbox();
+}
 
 // if ( $(window).width() < 767){
 // 	disableLightbox();
